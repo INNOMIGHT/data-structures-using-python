f922f55cc96150b5f16865cfa5fabb790d65ec9f
Vaibhav Shrivastava <iammagnificient@gmail.com>
2020-08-19 15:25:29 +0530

    Stack Implementation Using Python

---
+++ StackImplementation.py
@@ -0,0 +1,26 @@
+class Stack():
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+
+s = Stack()
+s.push(2)
+s.push(5)
+
+print(s.size())
