a40096a40d4fc631efaff69499f25abebcac194f
Vaibhav Shrivastava <iammagnificient@gmail.com>
2020-08-19 18:04:22 +0530

    Deque Implementation Using Python

---
+++ DequeImplementation.py
@@ -0,0 +1,28 @@
+class Deque(object):
+
+    def __init__(self):
+        self.items = []
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0, item)
+
+    def removeFront(self):
+        self.items.pop()
+
+    def removeRear(self):
+        self.items.pop(0)
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+    def printDeque(self):
+        print(self.items)
+
+
+dq = Deque()
